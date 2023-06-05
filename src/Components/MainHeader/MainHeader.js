6f2f7dd830a11947754d0cda9a6d8d702e840aad
Siddhant <siddhantchoudhary.omi2002@gmail.com>
2023-06-05 20:24:27 +0530

    new commit

---
+++ src/Components/MainHeader/MainHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './MainHeader.module.css';
+import Navigation from "./Navigation";
+const MainHeader = props => {
+    return (
+        <header className={classes['main-header']}>
+            <h1>A Typical Page</h1>
+            <Navigation></Navigation>
+        </header>
+    )
+}
+export default MainHeader;
