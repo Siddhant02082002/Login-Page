a70f2b40b7ade90a80bca69d6953047b46e40ec1
Siddhant <siddhantchoudhary.omi2002@gmail.com>
2023-06-05 20:21:31 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useContext} from "react";
+import "./App.css";
+import MainHeader from "./Components/MainHeader/MainHeader";
+import Login from "./Components/Login/Login";
+import Home from "./Components/Home/Home";
+import AuthContext from "./store/auth-context";
 function App() {
+  const ctx = useContext(AuthContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <MainHeader></MainHeader>
+      <main>
+        {!ctx.isLoggedIn && <Login></Login>}
+        {ctx.isLoggedIn && <Home></Home>}
+      </main>
+    </React.Fragment>
   );
 }
 
